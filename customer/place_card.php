53f6b81344b0c4fe901ff60b87d6501f22ade9ec
JADAV ANIKET KAMLESHKUMAR <86431201+ANIKET1074@users.noreply.github.com>
2023-10-21 19:11:29 +0530

    Places card layout added

---
+++ customer/place_card.php
@@ -0,0 +1,8 @@
+<a href="./placedetail.php?city='<?php echo $row['city'] ?>'" class="card flex items-center mt-[1.2rem] justify-between bg-[white] shadow-xl hover:shadow-2xl p-4 rounded-[0.8rem] border w-[20rem] h-[4rem]">
+    <span class="flex justify-center text-[1.3rem]">
+        <?php echo $row['city'] ?> Kitchens
+    </span>
+    <span class="text-[1.3rem]">
+        >
+    </span>
+</a>
